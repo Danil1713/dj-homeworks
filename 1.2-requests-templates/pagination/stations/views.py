fcbb3fed6ecb51ef568eac6caba80db313cd0ca1
Danil <danilderbassov10313@gmail.com>
2026-09-16 15:34:56 +0300

    Добавлена пагинация списка остановок

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from csv import DictReader
+from django.conf import settings
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -7,11 +10,19 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    with open(
+        settings.BUS_STATION_CSV,
+        encoding='utf-8',
+        newline='',
+    ) as file:
+        stations = list(DictReader(file))
+
+    paginator = Paginator(stations, 10)
+    page_number = request.GET.get('page', '1')
+    page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page.object_list,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
