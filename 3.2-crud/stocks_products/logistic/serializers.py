92eb6d82b3cb9fdda5b5d9948ce5e3cb764cd91e
Danil <danilderbassov10313@gmail.com>
2026-09-18 01:58:55 +0300

    Реализован CRUD товаров и складов с поиском и пагинацией

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,43 +1,68 @@
+from django.db import transaction
 from rest_framework import serializers
+
+from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
+    def validate_positions(self, positions):
+        product_ids = [
+            position['product'].pk
+            for position in positions
+        ]
+
+        if len(product_ids) != len(set(product_ids)):
+            raise serializers.ValidationError(
+                'Товар не должен повторяться в списке позиций.'
+            )
+
+        return positions
+
+    @transaction.atomic
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                **position,
+            )
 
         return stock
 
+    @transaction.atomic
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
+        positions = validated_data.pop('positions', None)
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        if positions is not None:
+            for position in positions:
+                StockProduct.objects.update_or_create(
+                    stock=stock,
+                    product=position['product'],
+                    defaults={
+                        'quantity': position['quantity'],
+                        'price': position['price'],
+                    },
+                )
 
         return stock
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,26 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.order_by('id')
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
-    queryset = Stock.objects.all()
+    queryset = (
+        Stock.objects
+        .order_by('id')
+        .prefetch_related('positions__product')
+    )
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
