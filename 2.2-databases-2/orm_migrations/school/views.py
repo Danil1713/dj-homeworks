5b11541cf82adb9c3a7f53afdbd33a53db744472
Danil <danilderbassov10313@gmail.com>
2026-09-17 13:47:37 +0300

    Изменена связь учеников и учителей на многие ко многим

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,11 +5,13 @@ from .models import Student
 
 
 def students_list(request):
-    template = 'school/students_list.html'
-    context = {}
+    students = (
+        Student.objects
+        .order_by('group', 'name')
+        .prefetch_related('teachers')
+    )
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
-    return render(request, template, context)
+    context = {
+        'object_list': students,
+    }
+    return render(request, 'school/students_list.html', context)
