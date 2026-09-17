501b7368b37d0d2a2b4d28133bacd227b15ed04c
Danil <danilderbassov10313@gmail.com>
2026-09-18 00:03:42 +0300

    Реализовано API датчиков и измерений температуры

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,36 @@
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+from .models import Measurement, Sensor
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+        read_only_fields = ['id']
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['id', 'sensor', 'temperature', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+
+class MeasurementDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'created_at']
+        read_only_fields = ['temperature', 'created_at']
+
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementDetailSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
+        read_only_fields = ['id']
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,27 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+)
+
+from .models import Sensor
+from .serializers import (
+    MeasurementSerializer,
+    SensorDetailSerializer,
+    SensorSerializer,
+)
+
+
+class SensorListCreateView(ListCreateAPIView):
+    queryset = Sensor.objects.order_by('id')
+    serializer_class = SensorSerializer
+    pagination_class = None
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.prefetch_related('measurements')
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementCreateView(CreateAPIView):
+    serializer_class = MeasurementSerializer
